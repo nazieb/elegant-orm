7b03cd4332ba92708e1868ca7b608c43c65c9698
Yavor Kirov <yavor99@gmail.com>
2014-05-08 12:25:54 +0300

    [fix] [ugly] use only one connection to db
    I downloaded your library and I liked it a lot. Sadly it was making 1 db connection for each model call. So my web host kept returning "too many connections to db" error. So I made this (yes, it's ugly) fix to make it work with only one connection. I'd love to update it in my project if you have a prettier solution.

--- src/querybuilder.php
+++ src/querybuilder.php
@@ -1,57 +1,58 @@
-<?php namespace Elegant;
+<?php
+
+namespace Elegant;
+
 class QueryBuilder {
 
-	protected $db_conn = null;
-	protected $table = '';
+    protected $db_conn = null;
+    protected $table = '';
 
-	function __construct($db_group, $table)
-	{
-		$ci =& get_instance();
+    function __construct($db_group, $table) {
+        global $__CI;
+        if (!isset($__CI)) {
+            $__CI =& get_instance();
+        }
 
-		$this->db_conn = $ci->load->database($db_group, true);
 
-		$this->table = $table;
-		$this->db_conn->from( $this->table );
+        $this->db_conn = $__CI->db;
 
-		if(defined('ELEGANT_DEBUG') and ELEGANT_DEBUG === true)
-		{
-			$property_name = 'elegant_db_'.rand();
-			$ci->{$property_name} = $this->db_conn;
-		}
-	}
+        $this->table = $table;
+        $this->db_conn->from($this->table);
 
-	function __call($name, $arguments)
-	{
-		if(!method_exists($this->db_conn, $name)) return show_error('Unknown function '.$name, 500);
+//        if (defined('ELEGANT_DEBUG') and ELEGANT_DEBUG === true) {
+////            $property_name = 'elegant_db_' . rand();
+////            $ci->asd = $this->db_conn;
+//        }
+    }
 
-		return call_user_func_array( array($this->db_conn, $name), $arguments );
-	}
+    function __call($name, $arguments) {
+        if (!method_exists($this->db_conn, $name))
+            return show_error('Unknown function ' . $name, 500);
 
-	function __clone()
-	{
-		$this->db_conn = clone $this->db_conn;
-	}
+        return call_user_func_array(array($this->db_conn, $name), $arguments);
+    }
 
-	function select($columns = array())
-	{
-		if (empty($columns)) $columns = '*';
+    function select($columns = array()) {
+        if (empty($columns))
+            $columns = '*';
 
-		elseif (is_array($columns)) $columns = implode(', ', $columns);
+        elseif (is_array($columns))
+            $columns = implode(', ', $columns);
 
-		$this->db_conn->select($columns);
-	}
+        $this->db_conn->select($columns);
+    }
 
-	function insert($data)
-	{
-		$insert = $this->db_conn->insert( $this->table, $data );
-		return ($insert !== false) ? $this->db_conn->insert_id() : false;
-	}
+    function insert($data) {
+        $insert = $this->db_conn->insert($this->table, $data);
+        return ($insert !== false) ? $this->db_conn->insert_id() : false;
+    }
 
-	function update($data, $where = array())
-	{
-		if(empty($where) and !is_array($where)) $where = array();
+    function update($data, $where = array()) {
+        if (empty($where) and ! is_array($where))
+            $where = array();
 
-		$update = $this->db_conn->update( $this->table, $data, $where );
-		return $update;
-	}
+        $update = $this->db_conn->update($this->table, $data, $where);
+        return $update;
+    }
+
 }
